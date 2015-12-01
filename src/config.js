983f75d7ce89ba5e1344daa45ecaaa3baae4ed1f
Sean Powell <sean@longdivision.co.uk>
2015-12-01 12:46:38 +0000

    Add comments URL to response items.

---
+++ src/config.js
@@ -0,0 +1,5 @@
+var config = {
+  COMMENT_BASE_URL: 'https://news.ycombinator.com/item?id='
+};
+
+module.exports = config;
--- src/net/firebase/marshallers/marshallStory.js
+++ src/net/firebase/marshallers/marshallStory.js
@@ -1,18 +1,18 @@
+var config = require('../../../config');
 var getDomainFromUrl = require('./utils/getDomainFromUrl');
 
 var marshallStoryOverview = function(responseData) {
   responseData = responseData || {};
   var item = {};
-
   var fullUrl = responseData.url || '';
   var domain = getDomainFromUrl(fullUrl);
-
   var currentUnixTime = Math.round(new Date() / 1000);
   var itemUnixTime = responseData.time || currentUnixTime;
 
   item.id = responseData.id || '-1';
   item.title = responseData.title || '';
   item.url = fullUrl;
+  item.commentsUrl = config.COMMENT_BASE_URL + item.id;
   item.author = responseData.by || '';
   item.commentCount = responseData.descendants || 0;
   item.domain = domain;
