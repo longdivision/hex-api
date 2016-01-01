a6a311ee5d2531ac45e41ffcf2d348dded479fe3
Sean Powell <sean@longdivision.co.uk>
2016-01-01 20:58:49 +0000

    Use in-memory caching for responses.

--- src/controllers/frontPage.js
+++ src/controllers/frontPage.js
@@ -1,9 +1,18 @@
 var getFrontPage = require('../service/getFrontPage');
 
-var frontPage = function (apiClientFactory, request, response) {
-  getFrontPage(apiClientFactory).then(function(frontPage) {
-    response.json(frontPage);
-  });
+const CACHE_KEY = 'FRONT_PAGE';
+
+var frontPage = function(apiClientFactory, cache, request, response) {
+  var cachedFrontPage = cache.get(CACHE_KEY);
+
+  if (cachedFrontPage) {
+    response.json(cachedFrontPage);
+  } else {
+    getFrontPage(apiClientFactory).then(function(frontPage) {
+      cache.set(CACHE_KEY, frontPage);
+      response.json(frontPage);
+    });
+  }
 };
 
 module.exports = frontPage;
--- src/controllers/story.js
+++ src/controllers/story.js
@@ -1,10 +1,23 @@
 var getStory = require('../service/getStory');
 
-var story = function (apiClientFactory, request, response) {
-  if(!request.params.id) {
+const CACHE_KEY_PREFIX = 'STORY_';
+
+var story = function(apiClientFactory, cache, request, response) {
+  var id = request.params.id;
+  var cacheKey = CACHE_KEY_PREFIX + id;
+
+  if(!id) {
     response.sendStatus(404);
+    return;
+  }
+
+  var cachedStory = cache.get(cacheKey);
+
+  if (cachedStory) {
+    response.json(cachedStory);
   } else {
     getStory(apiClientFactory, request.params.id).then(function(story) {
+      cache.set(cacheKey, story);
       response.json(story);
     });
   }
