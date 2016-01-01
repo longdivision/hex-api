2a4634099f568f2c576f5e4ccfc2876446763f41
Sean Powell <sean@longdivision.co.uk>
2016-01-01 21:54:37 +0000

    Warm the cache on startup and at 4 minute intervals.

---
+++ src/cache/cacheWarmer.js
@@ -0,0 +1,18 @@
+var apiClientFactory = require('../factory/apiClientFactory');
+var getFrontPage = require('../service/getFrontPage');
+var getStory = require('../service/getStory');
+
+var cacheWarmer = function(cache) {
+  getFrontPage(apiClientFactory).then(function(frontPage) {
+    cache.set('FRONT_PAGE', frontPage);
+
+    frontPage.forEach(function(frontPageStory) {
+      getStory(apiClientFactory, frontPageStory.id).then(function(story) {
+        var cacheKey = 'STORY_' + frontPageStory.id;
+        cache.set(cacheKey, story);
+      });
+    });
+  });
+};
+
+module.exports = cacheWarmer;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 if (process.env.NEW_RELIC_LICENSE_KEY) { require('newrelic'); }
 
 var NodeCache = require('node-cache');
+var schedule = require('node-schedule');
+
+var appFactory = require('./appFactory');
+var cacheWarmer = require('./cache/cacheWarmer');
 
 var cache = new NodeCache({ stdTTL: 300, checkperiod: 60});
-var appFactory = require('./appFactory');
+cacheWarmer(cache);
 
 var app = appFactory(cache);
 
 app.listen(app.get('port'), function() {
   console.log('Hex API is running on port', app.get('port'));
+});
+
+schedule.scheduleJob('*/4 * * * *', function() {
+  cacheWarmer(cache);
 });
