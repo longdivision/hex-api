b11d9d262ff0eb7917a261e1317192e986ff9a68
Sean Powell <sean@longdivision.co.uk>
2016-09-18 16:40:24 +0100

    Upgrade dependencies. Upgrade to Node v6.6.

--- src/net/firebase/clientFactory.js
+++ src/net/firebase/clientFactory.js
@@ -1,12 +1,15 @@
-var Firebase = require('firebase')
+var firebase = require('firebase')
 
-var API_URL = 'https://hacker-news.firebaseio.com/v0/'
+var API_URL = 'https://hacker-news.firebaseio.com'
 var client
 
 var clientFactory = {
   getClient: function () {
+    var config = {databaseURL: API_URL}
+    var version = '/v0'
+
     if (!client) {
-      client = new Firebase(API_URL)
+      client = firebase.initializeApp(config).database().ref(version)
     }
 
     return client
