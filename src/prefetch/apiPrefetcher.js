6951a6e53fcc3fa051085f54e5bd5885d903d01a
Sean Powell <sean@longdivision.co.uk>
2016-09-28 01:07:39 +0100

    Push HN API prefetching into a different process.

--- src/prefetch/apiPrefetcher.js
+++ src/prefetch/apiPrefetcher.js
@@ -1,6 +1,10 @@
+var redis = require('redis')
+var logger = require('js-logger')
 var clientFactory = require('../net/firebase/clientFactory')
 var getStoryCollection = require('../net/firebase/getStoryCollection')
 var getStoryWithComments = require('../net/firebase/getStoryWithComments')
+
+logger.useDefaults();
 
 const COLLECTION_NAME_TO_CACHE_KEY = {
   'askstories': 'ASK',
@@ -12,8 +16,18 @@ const COLLECTION_NAME_TO_CACHE_KEY = {
 
 const ITEMS_TO_LOAD = 100
 
-var cacheWarmer = function (cache) {
-  var firebaseClient = clientFactory.getClient()
+var cacheData = {};
+var cache = {
+  get: function(key) {
+    return cacheData[key];
+  },
+  set: function(key, value) {
+    cacheData[key] = value;
+  }
+}
+
+var apiPrefetcher = function () {
+  var firebaseClient = clientFactory.getClient(process.env.REDIS_URL)
 
   Object.keys(COLLECTION_NAME_TO_CACHE_KEY).forEach(function(collectionName) {
     getStoryCollection(firebaseClient, collectionName, ITEMS_TO_LOAD)
@@ -29,7 +43,17 @@ var cacheWarmer = function (cache) {
           })
       })
   })
-
 }
 
-module.exports = cacheWarmer
+logger.log('HN API prefetcher running');
+
+var redisClient = redis.createClient();
+var ONE_MINUTE_IN_MS = 60 * 1000;
+
+setInterval(function() {
+  apiPrefetcher();
+  redisClient.set("hex-api-data", JSON.stringify(cacheData));
+  logger.info('Published prefetched data from HN API')
+}, ONE_MINUTE_IN_MS);
+
+apiPrefetcher();
